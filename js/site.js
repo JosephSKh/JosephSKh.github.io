8f594639db3182edd92973dc75e0f9acf54fb6a5
JosephSKh <josephskhella@gmail.com>
2016-07-15 16:53:28 +0200

    Add files via upload

--- js/site.js
+++ js/site.js
@@ -3,9 +3,11 @@ $(document).ready(function(){
 		if($(window).scrollTop()>=$(".cover").outerHeight()){
 			$(".main-menu").addClass("navbar-fixed-top");
 			$(".about").css("padding-top",$(".main-menu").height());
+			$(".go-up").css("opacity",1);
 		}else{
 			$(".main-menu").removeClass("navbar-fixed-top");
 			$(".about").css("padding-top",0);
+			$(".go-up").css("opacity",0);
 		}
 	});
 	$(".mouse-icon").click(function(){
@@ -13,7 +15,7 @@ $(document).ready(function(){
 		    scrollTop: $(".cover").outerHeight()
 		 }, 1000);
 	});
-	$(".main-menu .navbar-brand").click(function(e){
+	$(".main-menu .navbar-brand,.go-up").click(function(e){
 		e.preventDefault();
 		$('html, body').stop().animate({
 		    scrollTop: 0
