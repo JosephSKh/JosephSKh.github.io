4a183f707b2994a8c05052283d3b15f1a9297e1f
JosephSKh <josephskhella@gmail.com>
2016-07-18 16:08:15 +0200

    Add files via upload

--- js/site.js
+++ js/site.js
@@ -21,11 +21,49 @@ $(document).ready(function(){
 		    scrollTop: 0
 		 }, 1000);
 	});
-	$(".main-menu li a").click(function(e){
-		e.preventDefault();
-		var link = this;
-		$('html, body').stop().animate({
-		    scrollTop: $($(link).attr("href")).offset().top
-		 }, 1000);
-	});
+
+
+
+    $(document).on("scroll", onScroll);
+    
+    //smoothscroll
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+        
+        $('a').each(function () {
+            $(this).removeClass('active');
+        })
+        $(this).addClass('active');
+      
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top+2
+        }, 500, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
+
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('.main-menu a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if(typeof refElement != 'undefined' && refElement.selector!="#"){
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('.main-menu ul li a').removeClass("active");
+            currLink.parent().addClass("active");
+        }
+        else{
+            currLink.parent().removeClass("active");
+        }
+        }
+    });
+}
+
+
+
 });
