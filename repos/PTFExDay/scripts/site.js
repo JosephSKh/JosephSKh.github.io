40dcc745436d311682568c40c21cc5cd5ab6be09
Joseph Safwat Khella <josephskhella@gmail.com>
2017-03-22 16:07:56 +0200

    Add files via upload

--- repos/PTFExDay/scripts/site.js
+++ repos/PTFExDay/scripts/site.js
@@ -2,17 +2,25 @@ var body = $("html, body");
 body.stop().animate({scrollTop:0}, '1000', 'swing', function() {
 });
 $(".navigation li").click(function(){
-    $(".navigation li").removeClass('active');
-    $(this).addClass('active');
+    // $(".navigation li").removeClass('active');
+    // $(this).addClass('active');
     var target = $(this).data('section');
     target = $("." + target).offset().top;
         body.stop().animate({scrollTop: target}, '1000', 'swing', function() {
     });
 });
 $(".line-container").click(function(){
-    $(".navigation li").removeClass('active');
-    $($(".navigation li")[1]).addClass('active');
+    // $(".navigation li").removeClass('active');
+    // $($(".navigation li")[1]).addClass('active');
     var target = $(".content").offset().top;
         body.stop().animate({scrollTop: target}, '1000', 'swing', function() {
     });
 });
+// $(window).scroll(function(){
+//     $("section").each(function(){
+//         if(window.scrollY >= $(this).offset().top && window.screenY <= $(this).offset().top + ($(this).height() - 500)) {
+//             $(".navigation li").removeClass('active');
+//             $("li[data-section="+$(this).data('name')+"]").addClass('active');
+//         }
+//     });
+// });
