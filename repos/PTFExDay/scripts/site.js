b36f0d585e30058f49931010cdf7ad4042a35bbd
Joseph Safwat Khella <josephskhella@gmail.com>
2017-03-22 15:51:51 +0200

    Add files via upload

--- repos/PTFExDay/scripts/site.js
+++ repos/PTFExDay/scripts/site.js
@@ -1,1 +1,18 @@
-console.log("works");
+var body = $("html, body");
+body.stop().animate({scrollTop:0}, '1000', 'swing', function() {
+});
+$(".navigation li").click(function(){
+    $(".navigation li").removeClass('active');
+    $(this).addClass('active');
+    var target = $(this).data('section');
+    target = $("." + target).offset().top;
+        body.stop().animate({scrollTop: target}, '1000', 'swing', function() {
+    });
+});
+$(".line-container").click(function(){
+    $(".navigation li").removeClass('active');
+    $($(".navigation li")[1]).addClass('active');
+    var target = $(".content").offset().top;
+        body.stop().animate({scrollTop: target}, '1000', 'swing', function() {
+    });
+});
